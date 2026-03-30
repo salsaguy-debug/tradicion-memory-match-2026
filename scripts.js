478dc137b4cc998a7c28d13b5455eb79a460701d
Salsa Guy Richmond <rodriguez2113@gmail.com>
2026-03-30 11:16:38 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,7 +12,9 @@ window.addEventListener('load', () => {
   const loadingScreen = document.getElementById('loading-screen');
   setTimeout(() => {
     loadingScreen.style.opacity = '0';
-    setTimeout(() => { loadingScreen.style.display = 'none'; }, 500);
+    setTimeout(() => {
+      loadingScreen.style.display = 'none';
+    }, 500);
   }, 1000); 
 });
 
